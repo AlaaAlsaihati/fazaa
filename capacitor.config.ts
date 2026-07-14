2e6521c516da1d5d685289b95cf653556de4db35
AlaaAlsaihati <njm7alaa@outlook.com>
2026-07-14 04:17:52 +0300

    app 2026

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.fazaa.app",
+  appId: "com.fazaa.app2026",
   appName: "Fazaa",
   webDir: "out",
 };
